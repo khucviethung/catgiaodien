b38cbf2e464b14bb444c880daa3883bc3e35aff1
Việt Hùngg <viethung.khuc@gmail.com>
2024-05-31 00:46:55 +0700

    improve/Homepage

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var myCarousel = document.querySelector('#carouselExampleInterval');
+    var myCarousel = document.querySelector('#carouselInterval');
     var carousel = new bootstrap.Carousel(myCarousel, {
       interval: false, // Disable auto-slide
     });
@@ -15,12 +15,16 @@ document.addEventListener('DOMContentLoaded', function() {
   
     // Update page information
     myCarousel.addEventListener('slid.bs.carousel', function(event) {
-      var totalSlides = document.querySelectorAll('#carouselExampleInterval .carousel-item').length;
+      var totalSlides = document.querySelectorAll('#carouselInterval .carousel-item').length;
       var currentSlide = event.to + 1; // 'to' gives the zero-based index of the current slide
       document.getElementById('page-info').textContent = 'Page ' + currentSlide + ' of ' + totalSlides;
     });
   
     // Initialize page information
-    var totalSlides = document.querySelectorAll('#carouselExampleInterval .carousel-item').length;
+    var totalSlides = document.querySelectorAll('#carouselInterval .carousel-item').length;
     document.getElementById('page-info').textContent = 'Page 1 of ' + totalSlides;
   });
+
+
+  // handle drag
+  
