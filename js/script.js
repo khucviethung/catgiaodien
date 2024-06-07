7c76ef9e34f4deae3fa8c5795a8be5bd2a7554a1
Việt Hùngg <viethung.khuc@gmail.com>
2024-06-07 16:53:13 +0700

    improve/homePage

--- js/script.js
+++ js/script.js
@@ -1,42 +1,42 @@
-document.addEventListener('DOMContentLoaded', () => {
-    var myCarousel = document.querySelector('#carouselInterval');
-    var carousel = new bootstrap.Carousel(myCarousel, {
-      interval: false, // Disable auto-slide
-    });
-  
-    // Custom navigation buttons
-    document.getElementById('prev-btn').addEventListener('click', () => {
-      carousel.prev();
-    });
-  
-    document.getElementById('next-btn').addEventListener('click', () => {
-      carousel.next();
-    });
+    // JS EVENT của carousel
+    document.addEventListener('DOMContentLoaded', () => {
+        var myCarousel = document.querySelector('#carouselInterval');
+        var carousel = new bootstrap.Carousel(myCarousel, {
+        interval: false, // Disable auto-slide
+        });
+    
+        // Custom navigation buttons
+        document.getElementById('prev-btn').addEventListener('click', () => {
+        carousel.prev();
+        });
+    
+        document.getElementById('next-btn').addEventListener('click', () => {
+        carousel.next();
+        });
   
 
     
-    // page navigation
+    // JS CHUYỂN TRANG page navigation
     // Lấy tất cả các phần tử left-content
-    const sections = document.querySelectorAll('.row-container');
-    const controlNavigation = document.getElementById('#pagination-controls')
+    const acticles = document.querySelectorAll('#articles-of-leftcontent article');
     const pageInfo = document.getElementById('page-info');
     const buttonNavigation = document.getElementById('buttonNavigation');
     let currentPage = 1;
     const itemsPerPage = 4;
-    const totalPages = Math.ceil(sections.length / itemsPerPage);
+    const totalPages = Math.ceil(acticles.length / itemsPerPage);
 
    
     function activePage(page) {
-        sections.forEach((section, index) => {
-            section.style.display = 'none';
+        acticles.forEach((acticle, index) => {
+            acticle.style.display = 'none';
             const start = (page - 1) * itemsPerPage;
             const end = page * itemsPerPage;
             if (index >= start && index < end ) {
-                section.style.display = 'flex';
+                acticle.style.display = 'block';    // tạo thành 1 block mới khi ở trang mới
                 buttonNavigation.textContent = "Previous";
             }
             else{
-                section.style.display = 'hidden'
+                acticle.style.display = 'hidden'
                 buttonNavigation.textContent = "Next"     
             }
         });
@@ -44,16 +44,23 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     buttonNavigation.addEventListener('click', () => {
-        if (currentPage < totalPages) {
-            currentPage ++ ;
-        }
-        else if(currentPage > totalPages){
-            currentPage -- ;
-        }
-        else {    
-            currentPage = 1;
-        }
-        activePage(currentPage);
+        buttonNavigation.disabled = true; // Vô hiệu hóa nút khi đã bấm nút 1 lần
+
+        // set timeout delay khi chuyển trang
+        setTimeout(() => {
+            if (currentPage < totalPages) {
+                currentPage ++;
+                buttonNavigation.textContent = "Previous";
+                console.log(`page ${currentPage}`, "Clicked to page 2");
+            } else if (currentPage === totalPages) {
+                currentPage = 1;
+                console.log(`page ${currentPage}`, "Back to page 1");
+                buttonNavigation.textContent = "Next";
+            }
+            activePage(currentPage);
+            window.scrollTo({ top: 0, behavior: 'smooth' }); // Scoll to top when change page
+            buttonNavigation.disabled = false; // Enable nút sau khi delay
+        }, 600); // 0,6s delay
         
     });
 
