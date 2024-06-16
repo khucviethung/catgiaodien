e1c7531bc019be69880dbe2038cb57fd47705408
Việt Hùngg <viethung.khuc@gmail.com>
2024-06-17 00:03:01 +0700

    update/membership

--- js/membership.js
+++ js/membership.js
@@ -1,23 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const monthlyRadio = document.getElementById("monthly");
-    const yearlyRadio = document.getElementById("yearly");
-    const monthlyContent = document.getElementById("monthly-content");
-    const yearlyContent = document.getElementById("yearly-content");
-  
-    function updateContent() {
-      if (monthlyRadio.checked) {
-        monthlyContent.style.display = "flex";
-        yearlyContent.style.display = "none";
-      } else {
-        yearlyContent.style.display = "flex";
-        monthlyContent.style.display = "none";
-      }
-    }
-  
-    monthlyRadio.addEventListener("change", updateContent);
-    yearlyRadio.addEventListener("change", updateContent);
-  
-    // Initialize content display based on initial radio button state
-    updateContent();
+document.addEventListener('DOMContentLoaded', () => {
+  const monthlyBtn = document.getElementById('monthly-btn');
+  const yearlyBtn = document.getElementById('yearly-btn');
+  const monthlyPricing = document.getElementById('monthly');
+  const yearlyPricing = document.getElementById('yearly');
+
+  monthlyBtn.addEventListener('click', () => {
+    monthlyBtn.classList.add('active');       // thêm nút monthly vào khi click
+    yearlyBtn.classList.remove('active');     // remove yearly button khi đã click monthly
+    monthlyPricing.classList.add('active');   // thêm content vào khi bấm monthly
+    yearlyPricing.classList.remove('active'); // remove monthly content khi đã click monthly
   });
+
+  yearlyBtn.addEventListener('click', () => {
+    yearlyBtn.classList.add('active');         // ngược lại 
+    monthlyBtn.classList.remove('active');
+    yearlyPricing.classList.add('active');
+    monthlyPricing.classList.remove('active');
+  });
+
   
+});
